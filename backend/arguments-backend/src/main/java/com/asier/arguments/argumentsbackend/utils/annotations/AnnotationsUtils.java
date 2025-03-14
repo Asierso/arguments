98367309593e19dfdf19374e5ee8d41022201b08
Asierso <33327643+Asierso@users.noreply.github.com>
2025-03-14 18:28:45 +0100

    Javadoc

--- backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/utils/annotations/AnnotationsUtils.java
+++ backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/utils/annotations/AnnotationsUtils.java
@@ -1,21 +1,29 @@
 package com.asier.arguments.argumentsbackend.utils.annotations;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.lang.reflect.Field;
 import java.util.*;
 
+@Slf4j
 public class AnnotationsUtils {
-    public static boolean isValidEntity(Object entity){
-        if(entity == null){
+    /**
+     * Check if the provided entity is valid. A valid entity must have all fields annotated with @Mandatory filled
+     * @param entity Entity to check
+     * @return If the entity is valid
+     */
+    public static boolean isValidEntity(Object entity) {
+        if (entity == null) {
             return false;
         }
 
         boolean valid = true;
 
-        for(Iterator<?> it = Arrays.asList(entity.getClass().getDeclaredFields()).iterator(); it.hasNext() && valid; ){
-            Field field = (Field)it.next();
+        for (Iterator<?> it = Arrays.asList(entity.getClass().getDeclaredFields()).iterator(); it.hasNext() && valid; ) {
+            Field field = (Field) it.next();
             field.setAccessible(true);
             try {
-                if(field.isAnnotationPresent(Mandatory.class) && field.get(entity) == null){
+                if (field.isAnnotationPresent(Mandatory.class) && field.get(entity) == null) {
                     valid = false;
                 }
             } catch (IllegalAccessException e) {
@@ -25,8 +33,24 @@ public class AnnotationsUtils {
         return valid;
     }
 
-    public static <T> void modifyEntity(T source, T changes){
-        if(source == null){
+    /**
+     * Check if the provided entity is not valid. A not valid entity may have at least one field annotated with @Mandatory like null
+     * @param entity Entity to check
+     * @return If the entity is not valid
+     */
+    public static boolean isNotValidEntity(Object entity){
+        return !isValidEntity(entity);
+    }
+
+    /**
+     * Modify source entity with provided changes. This method uses @Modifiable anotation to determine if the field can be overridden or not
+     *
+     * @param source  Source entity
+     * @param changes Same source entity with the changes to apply
+     * @param <T>     Entity class
+     */
+    public static <T> void modifyEntity(T source, T changes) {
+        if (source == null) {
             return;
         }
 
@@ -34,7 +58,7 @@ public class AnnotationsUtils {
             field.setAccessible(true);
             try {
                 if (field.isAnnotationPresent(Modifiable.class))
-                    if(field.get(changes) != null)
+                    if (field.get(changes) != null)
                         field.set(source, field.get(changes));
             } catch (IllegalAccessException e) {
                 log.error("Error at modifying entity {}", source.getClass().getSimpleName());
