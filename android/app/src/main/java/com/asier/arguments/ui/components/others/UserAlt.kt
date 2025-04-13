9b2fd6201558c5cb1aa4d8d712bd18ad98b6f58c
Asierso <33327643+Asierso@users.noreply.github.com>
2025-04-13 17:43:50 +0200

    Features: UerAlt pic component

---
+++ android/app/src/main/java/com/asier/arguments/ui/components/others/UserAlt.kt
@@ -0,0 +1,74 @@
+package com.asier.arguments.ui.components.others
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.intl.Locale
+import androidx.compose.ui.text.toUpperCase
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.asier.arguments.ui.theme.Montserrat
+import java.util.Objects
+import kotlin.random.Random
+
+@Composable
+fun UserAlt(
+    name: String,
+    actions: () -> Unit){
+    Box(
+        modifier = Modifier.clip(RoundedCornerShape(100.dp)).width(50.dp).height(50.dp).clickable { actions() }
+            .background(getColorByName(name)),
+
+        contentAlignment = Alignment.Center){
+        Text(
+            text = name.substring(0,2).toUpperCase(Locale.current),
+            fontSize = 20.sp,
+            color = Color.White,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.SemiBold)
+    }
+}
+
+fun getColorByName(name: String) : Color{
+    val hash = Objects.hash(name)
+    val rng = Random(hash)
+    return Color(
+        red = rng.nextInt(100,255),
+        green = rng.nextInt(100,255),
+        blue = rng.nextInt(100,255))
+}
+
+@Composable
+@Preview
+fun UserAltPreview0(){
+    UserAlt("Marco") { }
+}
+
+@Composable
+@Preview
+fun UserAltPreview1(){
+    UserAlt("Juan") { }
+}
+
+@Composable
+@Preview
+fun UserAltPreview2(){
+    UserAlt("Luis") { }
+}
+
+@Composable
+@Preview
+fun UserAltPreview3(){
+    UserAlt("Ruth") { }
+}
