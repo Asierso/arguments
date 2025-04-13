a76f73e0e1bac8ebe81b83aed09a69a1ab33c8b8
Asierso <33327643+Asierso@users.noreply.github.com>
2025-04-13 17:25:54 +0200

    Features: New top bar styles

--- android/app/src/main/java/com/asier/arguments/ui/components/topbars/BaseTopBar.kt
+++ android/app/src/main/java/com/asier/arguments/ui/components/topbars/BaseTopBar.kt
@@ -1,31 +1,43 @@
 package com.asier.arguments.ui.components.topbars
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.asier.arguments.ui.theme.Montserrat
+import com.asier.arguments.ui.theme.TextBright0
+import com.asier.arguments.ui.theme.TopBarBackground
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseTopBar(
-    title: String,
+    content: @Composable () -> Unit,
     modifier: Modifier = Modifier) {
     TopAppBar(
         title = {
-        Text(text = title,
-            textAlign = TextAlign.Center,
-            fontFamily = Montserrat)
+            content()
         },
         modifier = modifier,
+        colors = TopAppBarColors(
+            containerColor = TopBarBackground,
+            titleContentColor = TextBright0,
+            navigationIconContentColor = Color.White,
+            actionIconContentColor =  Color.White,
+            scrolledContainerColor = Color.Transparent
+        ),
         actions = {})
 }
 
 @Composable
 @Preview(showSystemUi = true)
 fun BaseTopBarPreview(){
-    BaseTopBar(title = "Topbar prev")
+    BaseTopBar(content = { Text("Base") })
 }
