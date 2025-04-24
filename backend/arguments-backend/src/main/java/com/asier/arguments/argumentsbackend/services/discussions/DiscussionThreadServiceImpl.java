7c6ba9f8a0538831e676e71942b5540a7e479181
Asierso <33327643+Asierso@users.noreply.github.com>
2025-04-24 19:56:46 +0200

    Fix: Fixed bad revert

--- backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/services/discussions/DiscussionThreadServiceImpl.java
+++ backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/services/discussions/DiscussionThreadServiceImpl.java
@@ -37,7 +37,7 @@ public class DiscussionThreadServiceImpl implements DiscussionThreadService {
     }
 
     public Page<DiscussionThread> findInPage(int page){
-        return discussionRepository.findAll(PageRequest.of(page,Integer.parseInt(props.getProperty("arguments.api.discussionsPerPage")), Sort.by("createdAt").descending()));
+        return discussionRepository.findAll(PageRequest.of(page,Integer.parseInt(props.getProperty("arguments.api.discussionsPerPage")), Sort.by("endAt").descending()));
     }
 
     @Override
