02c35d2284a8afd38227e7e4175d727098b4d28d
Asierso <33327643+Asierso@users.noreply.github.com>
2025-04-24 19:51:48 +0200

    Revert "Fix: Order by endAt (priorice biggest discussions and the ones who are active)"
    This reverts commit 7ee227e01d0f4d72a86d99825463c0bddd3ed3d5.
    
    Reverted changed

--- backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/services/discussions/DiscussionThreadServiceImpl.java
+++ backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/services/discussions/DiscussionThreadServiceImpl.java
@@ -35,7 +35,7 @@ public class DiscussionThreadServiceImpl implements DiscussionThreadService {
     }
 
     public Page<DiscussionThread> findInPage(int page){
-        return discussionRepository.findAll(PageRequest.of(page,Integer.parseInt(props.getProperty("arguments.api.discussionsPerPage")), Sort.by("endAt").descending()));
+        return discussionRepository.findAll(PageRequest.of(page,Integer.parseInt(props.getProperty("arguments.api.discussionsPerPage")), Sort.by("createdAt").descending()));
     }
 
     @Override
