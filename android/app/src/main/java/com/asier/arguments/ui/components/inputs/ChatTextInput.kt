93de63159a39e218d532cc685995b7e7f7edb4c5
Asierso <33327643+Asierso@users.noreply.github.com>
2025-05-02 19:01:55 +0200

    Features: ChatTextInput borders fixed

--- android/app/src/main/java/com/asier/arguments/ui/components/inputs/ChatTextInput.kt
+++ android/app/src/main/java/com/asier/arguments/ui/components/inputs/ChatTextInput.kt
@@ -1,5 +1,7 @@
 package com.asier.arguments.ui.components.inputs
 
+import android.graphics.Color
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,11 +12,16 @@ import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.asier.arguments.R
+import com.asier.arguments.ui.theme.TextBoxBorder
 import com.asier.arguments.ui.theme.TextBright1
 
 @Composable
@@ -30,7 +37,20 @@ fun ChatTextInput(
             placeholder = placeholder,
             text = text,
             onValueChanged = onValueChanged,
-            modifier = Modifier.fillMaxWidth().shadow(5.dp)
+            modifier = Modifier.fillMaxWidth().shadow(5.dp).drawBehind {
+                val strokeBottom = 2.dp.toPx()
+                val radius = 9.dp.toPx()
+                val width = size.width
+                val height = size.height-9
+
+
+                drawRoundRect(
+                    color = TextBoxBorder,
+                    topLeft = Offset(11f, 50f),
+                    size = Size(width-22f, height-strokeBottom-43f),
+                    cornerRadius = CornerRadius(radius, radius)
+                )
+            }
         )
         IconButton(
             onClick = {
