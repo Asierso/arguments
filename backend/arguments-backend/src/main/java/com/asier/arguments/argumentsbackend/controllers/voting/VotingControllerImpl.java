a01c9e6ff24cc511168e0d3edcda7302b64e1b69
Asierso <33327643+Asierso@users.noreply.github.com>
2025-05-02 21:51:39 +0200

    Fix: Fixed votation controller

--- backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/controllers/voting/VotingControllerImpl.java
+++ backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/controllers/voting/VotingControllerImpl.java
@@ -8,9 +8,11 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Properties;
 
+@RestController
 @RequestMapping("/api/v1/auth")
 public class VotingControllerImpl implements VotingController {
     @Autowired
