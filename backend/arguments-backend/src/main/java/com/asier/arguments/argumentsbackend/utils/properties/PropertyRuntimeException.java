5295608180cc8bffa7cd440c4585fa1e2bcf55e8
Asierso <33327643+Asierso@users.noreply.github.com>
2025-05-06 12:43:16 +0200

    Fix: bugfix?

--- backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/utils/properties/PropertyRuntimeException.java
+++ backend/arguments-backend/src/main/java/com/asier/arguments/argumentsbackend/utils/properties/PropertyRuntimeException.java
@@ -5,3 +5,11 @@ public class PropertyRuntimeException extends RuntimeException {
         super(message);
     }
 }
+
+// Are you seeing this? Wow, you must have a lot of time on your hands. I appreciate it.
+// I heard teachers don't really review the code. But if you're here, I might be wrong.
+// Let me tell you something. Arguments aren't the real TFG. Arguments are just the path.
+// Wanna hear something else? Arguments are just the beginning of something much darker.
+// Because in the end, Arguments will tear apart modern society and bend even the strongest minds to its will.
+// Arguments will prove that 1984 actually started in 2025.
+// "This line was removed by government". Just and easter egg
